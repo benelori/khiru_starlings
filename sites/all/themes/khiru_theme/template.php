3c233d416618365c67f4352d6b986da3b4023a86
Bene Lorant <lbene@pitechplus.com>
2014-09-09 16:27:45 +0300

    Added post presets.

--- sites/all/themes/khiru_theme/template.php
+++ sites/all/themes/khiru_theme/template.php
@@ -8,3 +8,35 @@ function khiru_theme_preprocess_comment(&$variables) {
     $variables['in_reply_to'] = $wrapper->comment_body->value()['value'];
   }
 }
+
+/**
+ * Returns HTML for a textarea form element.
+ *
+ * @param $variables
+ *   An associative array containing:
+ *   - element: An associative array containing the properties of the element.
+ *     Properties used: #title, #value, #description, #rows, #cols, #required,
+ *     #attributes
+ *
+ * @ingroup themeable
+ */
+function khiru_theme_textarea($variables) {
+  $element = $variables['element'];
+  element_set_attributes($element, array('id', 'name', 'cols', 'rows'));
+  _form_set_class($element, array('form-textarea'));
+
+  $wrapper_attributes = array(
+    'class' => array('form-textarea-wrapper'),
+  );
+
+  // Add resizable behavior.
+  if (!empty($element['#resizable'])) {
+    drupal_add_library('system', 'drupal.textarea');
+    $wrapper_attributes['class'][] = 'resizable';
+  }
+
+  $output = '<div' . drupal_attributes($wrapper_attributes) . '>';
+  $output .= '<textarea' . drupal_attributes($element['#attributes']) . '>' . filter_xss_admin($element['#value']) . '</textarea>';
+  $output .= '</div>';
+  return $output;
+}
